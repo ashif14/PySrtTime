79a5880c48db0549d7951715bcb4d03c5a07b7f8
MOHD ASHIF  ALI <1295519@India.TCS.com>
2017-08-21 16:52:54 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## PySrtTime
+Python Script to get Subtitle time duration
+
+## How to use
+
+```
+Call getSubtitleTime(filename) module to get time duration of subtitle track
+```
+## Version
+
+```
+1.0
+```
+
+## Future Committments
+Working on this to be more generic, it's just the beginning.
+
+## Author
+**Mohd Ashif Ali** - https://github.com/ashif14
---
+++ app.py
@@ -0,0 +1,44 @@
+import app
+
+import re
+
+def getSequenceTime(sequence):
+# time = '00:00:00,000 --> 00:00:04,440'
+
+	timeframe = sequence.split('-->')
+	timecodes = []
+
+	timecodes.append(timeframe[0].strip())
+	timecodes.append(timeframe[1].strip()) 
+
+	totalSecs = 0
+	for tm in timecodes:
+	    timeParts = [int(s) for s in tm.split(',')[0].split(':')]
+	    totalSecs += (timeParts[0] * 60 + timeParts[1]) * 60 + timeParts[2]
+	# totalSecs, sec = divmod(totalSecs, 60)
+	# hr, min = divmod(totalSecs, 60)
+	# print "%d:%02d:%02d" % (hr, min, sec)
+	return totalSecs
+
+def getSubtitleTime(file):
+	isSeq = False
+	firstSeq = False
+	firstTimeSeq = None
+	lastTimeSeq = None
+	with open(file) as f:
+	    for line in f:
+
+	    	if(firstSeq):
+	    		firstTimeSeq = line.strip()
+	    		firstSeq = False
+	    	if(line.strip() == '1'):
+	    		firstSeq = True
+	    	if(isSeq):
+	    		lastTimeSeq = line.strip()
+	    		isSeq = False
+	    	if(re.compile('[0-9]+$').match(line.strip())):
+	    		isSeq = True
+
+	return getSequenceTime(lastTimeSeq) - getSequenceTime(firstTimeSeq)
+
+# print(getSubtitleTime('sample.srt'))
